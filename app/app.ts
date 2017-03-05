dec87a0ac20411172cb1022343ed69dab244b756
Rui <rui.albuquerque.dorey@gmail.com>
2017-03-05 18:33:59 +0000

    adds systemjs, stats js

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,31 @@
+import * as Stats from '/stats-js/build/stats.min.js'
+
 class Test {
     user: string;
+    stats: any;
 
     greeter = (person) => {
         return "Hello, " + person;
     }
 
+    initStats = () => {
+        this.stats = new Stats();
+        this.stats.setMode(1); // 0: fps, 1: ms, 2: mb, 3+: custom
+
+        this.stats.domElement.style.position = 'absolute';
+        this.stats.domElement.style.left = '0px';
+        this.stats.domElement.style.top = '0px';
+
+    }
+
     constructor() {
         this.user = "Jane User";
         document.body.innerHTML = this.greeter(this.user);
+        this.initStats();
     }
 }
-
 let _test = new Test();
+
+// hack to put object on global context
+(<any>window)._test = _test;
+document.body.appendChild(_test.stats.domElement);
