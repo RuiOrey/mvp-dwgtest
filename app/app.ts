19ef0a4fd1fe777668bc58a5f48926d63cbee79b
Rui <rui.albuquerque.dorey@gmail.com>
2017-03-05 17:09:56 +0000

    update

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,14 @@
-let greeter = (person) => {
-    return "Hello, " + person;
+class Test {
+    user: string;
+
+    greeter = (person) => {
+        return "Hello, " + person;
+    }
+
+    constructor() {
+        this.user = "Jane User";
+        document.body.innerHTML = this.greeter(this.user);
+    }
 }
 
-let user = "Jane User";
-
-document.body.innerHTML = greeter(user);
+let _test = new Test();
