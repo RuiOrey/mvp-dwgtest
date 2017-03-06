e76a204ae83c3597e298c2df7740dbd30fa3f941
Rui <rui.albuquerque.dorey@gmail.com>
2017-03-06 08:00:40 +0000

    threejs init

--- app/app.ts
+++ app/app.ts
@@ -1,85 +1,105 @@
-import * as Stats from '/stats-js/build/stats.min.js'
+import * as Stats from 'stats-js/build/stats.min.js'
 import * as THREE from 'three/build/three.js'
 
 class Test {
+
     user: string;
     stats: any;
+    scene: any;
+    camera: any;
+    renderer: any;
+    light: any;
+    light2: any;
+    box: any;
+
 
     initThreeJS = () => {
 
+        let width = window.innerWidth;
+        let height = window.innerHeight;
+
+        console.log(window.innerWidth, window.innerHeight);
+        let cameraDimensionsDivisor = 128;
+
         // create the scene
-        let scene = new THREE.Scene()
+        this.scene = new THREE.Scene();
 
         // create the camera
-        let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+        //this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.camera = new THREE.OrthographicCamera(-width / cameraDimensionsDivisor, width / cameraDimensionsDivisor, height / cameraDimensionsDivisor, -height / cameraDimensionsDivisor, .1, 1000);
 
-        let renderer = new THREE.WebGLRenderer()
+
+        this.renderer = new THREE.WebGLRenderer();
 
         // set size
-        renderer.setSize(window.innerWidth, window.innerHeight)
+        this.renderer.setSize(width, height);
 
         // add canvas to dom
-        document.body.appendChild(renderer.domElement)
+        document.body.appendChild(this.renderer.domElement);
 
         // add axis to the scene
-        let axis = new THREE.AxisHelper(10)
+        let axis = new THREE.AxisHelper(10);
 
-        scene.add(axis)
+        this.scene.add(axis);
 
         // add lights
-        let light = new THREE.DirectionalLight(0xffffff, 1.0)
+        this.light = new THREE.DirectionalLight(0xffffff, 1.0);
 
-        light.position.set(100, 100, 100)
+        this.light.position.set(100, 100, 100);
 
-        scene.add(light)
+        this.scene.add(this.light);
 
-        let light2 = new THREE.DirectionalLight(0xffffff, 1.0)
+        this.light2 = new THREE.DirectionalLight(0xffffff, 1.0);
 
-        light2.position.set(-100, 100, -100)
+        this.light2.position.set(-100, 100, -100);
 
-        scene.add(light2)
+        this.scene.add(this.light2);
 
         let material = new THREE.MeshBasicMaterial({
             color: 0xaaaaaa,
             wireframe: true
-        })
+        });
 
         // create a box and add it to the scene
-        let box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material)
+        this.box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material);
 
-        scene.add(box)
+        this.scene.add(this.box);
 
-        box.position.x = 0.5
-        box.rotation.y = 0.5
+        this.box.position.x = 0.5;
+        this.box.rotation.y = 0.5;
 
-        camera.position.x = 5
-        camera.position.y = 5
-        camera.position.z = 5
+        this.camera.position.x = 5;
+        this.camera.position.y = 5;
+        this.camera.position.z = 5;
 
-        camera.lookAt(scene.position)
+        this.camera.lookAt(this.scene.position);
 
-        function animate(): void {
-            requestAnimationFrame(animate)
-            render()
-        }
 
-        function render(): void {
-            let timer = 0.002 * Date.now()
-            box.position.y = 0.5 + 0.5 * Math.sin(timer)
-            box.rotation.x += 0.1
-            renderer.render(scene, camera)
-        }
-
-        animate()
     }
 
-    greeter = (person) => {
-        return "Hello, " + person;
+
+    animate = () => {
+        requestAnimationFrame(()=> {
+            this.animate()
+        });
+        this.render();
     }
+
+    function
+
+    render = () => {
+        this.stats.begin();
+        let timer = 0.002 * Date.now();
+        this.box.position.y = 0.5 + 0.5 * Math.sin(timer);
+        this.box.rotation.x += 0.1;
+        this.stats.end();
+        this.renderer.render(this.scene, this.camera);
+    }
+
 
     initStats = () => {
         this.stats = new Stats();
-        this.stats.setMode(1); // 0: fps, 1: ms, 2: mb, 3+: custom
+        // this.stats.setMode(1); // 0: fps, 1: ms, 2: mb, 3+: custom
 
         this.stats.domElement.style.position = 'absolute';
         this.stats.domElement.style.left = '0px';
@@ -88,9 +108,10 @@ class Test {
     }
 
     constructor() {
-        this.user = "Jane User";
-        document.body.innerHTML = this.greeter(this.user);
+        this.initThreeJS();
         this.initStats();
+
+        this.animate();
     }
 }
 let _test = new Test();
